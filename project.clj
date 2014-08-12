1632acd2cded65e8a570dbe7202e0393adc754d3
Paul DeLong <pd72@nyu.edu>
2014-08-12 18:06:54 -0400

    added the command-line arg parsing logic, and the basic usage/help messages;

--- project.clj
+++ project.clj
@@ -1,3 +1,5 @@
 (defproject mandelbrojt "0.1.0-SNAPSHOT"
-   :dependencies [[org.clojure/clojure "1.5.1"]]
+   :dependencies
+   [  [org.clojure/clojure   "1.5.1"]
+      [org.clojure/tools.cli "0.3.1"]  ]
    :main mandelbrojt.core  )
--- src/mandelbrojt/core.clj
+++ src/mandelbrojt/core.clj
@@ -1,8 +1,37 @@
 (ns mandelbrojt.core
-   (:require [clojure.pprint :refer [pprint]])
+   (:require
+      [clojure.pprint    :refer [pprint]]
+      [clojure.string    :refer [join]]
+      [clojure.tools.cli :refer [parse-opts]]  )
    (:gen-class)  )
 
-(defn main-body
+(def cli-options
+   [  [  "-w"
+         "--width INT"
+         "width of grid in pixels"
+         :parse-fn #(Integer/parseInt %)
+         :validate [integer? "not an integer"]
+         :default 100  ]
+      [  "-h"
+         "--height INT"
+         "height of grid in pixels"
+         :parse-fn #(Integer/parseInt %)
+         :validate [integer? "not an integer"]
+         :default 100  ]
+      [  "-H" "--help" "help"  ]  ]  )
+
+(defn usage
+   [exit-code options-summary & [error-msg]]
+   (if error-msg (println error-msg "\n"))
+   (println
+      (join \newline
+         [  "usage: write me"
+            ""
+            "Options:"
+            options-summary  ]  )  )
+   (System/exit exit-code)  )
+
+(defn render-area
    [  [x-pixels y-pixels]
       [  [x-min x-max]
          [y-min y-max]  ]  ]
@@ -10,7 +39,15 @@
            y (range y-pixels)  ]
         [x y]  )  )
 
+(defn main-body
+   [  {  {:keys [width height help]} :options
+          :keys [arguments errors summary]  }  ]
+   (if help   (usage 0 summary errors))
+   (if errors (usage 1 summary errors))
+   ; translate from input parameters to grid bounds
+   (render-area [2 2] [[-2 +2] [-2 +2]])  )
+
 (defn -main
    [& args]
    (alter-var-root #'*read-eval* (constantly false)) ;; work around dangerous default behaviour in Clojure
-   (pprint (main-body [3 3] [[-2 +2] [-2 +2]]))  )
+   (pprint (main-body (parse-opts args cli-options)))  )
