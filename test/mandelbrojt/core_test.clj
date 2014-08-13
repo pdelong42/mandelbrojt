4b1487e995760fb6eb82ccc7bbd2a7531aae5a50
Paul DeLong <pd72@nyu.edu>
2014-08-13 17:51:14 -0400

    some basic tests to start with;

--- test/mandelbrojt/core_test.clj
+++ test/mandelbrojt/core_test.clj
@@ -3,4 +3,6 @@
       [clojure.test :refer :all]
       [mandelbrojt.core :refer :all]  )  )
 
-(deftest a-test (testing "FIXME, I fail." (is (= 0 1))))
+(deftest grid2x2 (testing "2x2 grid" (is (= #{[0 0] [0 1] [1 0] [1 1]} (set (render-rectangle [2 2] nil))))))
+
+(deftest grid3x3 (testing "3x3 grid" (is (= #{[0 0] [0 1] [0 2] [1 0] [1 1] [1 2] [2 0] [2 1] [2 2]} (set (render-rectangle [3 3] nil))))))
